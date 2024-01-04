f32cdf10fd4fa409cf866c6c612c40575be59d4e
Bianca <bianca.g.buha@gmail.com>
2024-01-04 11:07:11 +0000

    added console.log to test

--- Backend/models/review.models.js
+++ Backend/models/review.models.js
@@ -5,11 +5,13 @@ let reviews;
 
 export default class ReviewsModels {
     static async injectDB(conn) {
+        console.log(conn);
         if (reviews) {
             return
         }
         try {
             reviews = await conn.db("reviews").collection("reviews")
+            console.log(reviews)
         } catch (e) {
             console.error(`Unable to establish collection handles in userDAO: ${e}`)
         }
@@ -23,6 +25,7 @@ export default class ReviewsModels {
             review: review,
           }
           console.log("adding")
+          console.log(reviewDoc)
           return await reviews.insertOne(reviewDoc)
         } catch (e) {
           console.error(`Unable to post review: ${e}`)
