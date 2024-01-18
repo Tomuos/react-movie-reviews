2ef98b642fd1260a9986f7632a14e5f950ebf5a9
Bianca <bianca.g.buha@gmail.com>
2024-01-18 14:52:58 +0000

    added routes to App and started styling

--- Frontend/movie-reviewer/src/App.js
+++ Frontend/movie-reviewer/src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import Card from './components/Card/Card';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Movie Reviewer</h1>
+
+      <Routes>
+        <Route path="/" element={<Card />} />
+      </Routes>
     </div>
   );
 }
