6ea4726eb5c14aa38e6435be8b299165c66de26a
Tomuos <tomulvlup@gmail.com>
2024-02-08 10:33:20 +0000

    clean up on cards

--- Frontend/movie-reviewer/src/components/Cards/Cards.js
+++ Frontend/movie-reviewer/src/components/Cards/Cards.js
@@ -1,9 +1,8 @@
 import "./Cards.css";
 import "../Card/Card.js";
 import Card from "../Card/Card.js";
-import MovieReviews from "../MovieReviews/MovieReviews.js";
 import { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+
 import SearchMovie from "../Search/Search.js";
 
 const API_Key = process.env.REACT_APP_API_KEY;
@@ -14,10 +13,8 @@ const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=${API_Key}
 function Cards() {
     const [movies, setMovies] = useState(null);
     const [searchTerm, setSearchTerm] = useState('');
-    const [movie, setMovie] = useState(null);
-    const { id } = useParams();
+    
 
-    console.log(movie)
 
     async function getMovies(movie_Link) {
         const response = await fetch(movie_Link);
@@ -31,15 +28,7 @@ function Cards() {
     }
     , []);
 
-// console.log(movies);
 
-    async function getMovie(id) {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_Key}&language=en-US`);
-        const data = await response.json();
-        console.log(data, "movie data")
-        console.log(data.title, "movie title")
-        setMovie(data);
-}
 
     async function handleSearchMovies(e) {
         e.preventDefault();
@@ -52,11 +41,7 @@ function Cards() {
         }
     }
 
-    useEffect(() => {
-        if (id) {
-            getMovie(id);
-        }
-    }, [id]);
+
 
 
     getMovies();
@@ -73,11 +58,11 @@ function Cards() {
                     image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                     rating={movie.vote_average}
                     releaseDate={movie.release_date}
-                    handleSeeReviews={() => getMovie(movie.id)}
+                    
                 />
             ))}
             </div>
-            {movie && <MovieReviews movie={movie} />}
+
         </div>
     )
 }
