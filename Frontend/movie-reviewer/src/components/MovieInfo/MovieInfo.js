d3e5cdfcbd824c69225faf9f335f3ecfaf753265
Bianca <bianca.g.buha@gmail.com>
2024-03-12 11:11:37 +0000

    changed MovieInfo styling

--- Frontend/movie-reviewer/src/components/MovieInfo/MovieInfo.js
+++ Frontend/movie-reviewer/src/components/MovieInfo/MovieInfo.js
@@ -31,47 +31,50 @@ function MovieInfo() {
 
     return (
         <div className='movie-container'>
-            <h1>Movie Info</h1>
+            
 
-            <div className='grid-container'>
-                <div className='movie-details-poster'>
+            {/* <div className='grid-container'> */}
+                <div className='movie-details-poster-div'>
                     <div className='movie-poster'>
+                    <h1>Movie Info</h1>
                         {movieInfo && <img src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`} alt={movieInfo.title} />}
                     </div>
                     <div className='movie-details'>
                         {movieInfo && <h2>{movieInfo.title}</h2>}
                         {movieInfo && <p>{movieInfo.overview}</p>}
-                        {movieInfo && <p>Rating: {movieInfo.vote_average} / 10</p>}
-                        {movieInfo && <p>Released: {movieInfo.release_date}</p>}
-                        {movieInfo && <p>Runtime: {movieInfo.runtime} minutes</p>}
+                        {movieInfo && <p><span>Rating: </span>{movieInfo.vote_average} / 10</p>}
+                        {movieInfo && <p><span>Released: </span>{movieInfo.release_date}</p>}
+                        {movieInfo && <p><span>Runtime: </span>{movieInfo.runtime} minutes</p>}
                     </div>
                 </div>
 
-                <div className='trailer-movie-cast'>
+                <div className='trailer-movie-cast-div'>
                     <div className='trailer'>
-                        {video && <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.results[video.results.length-1].key}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>}
+                        {video && <iframe src={`https://www.youtube.com/embed/${video.results[video.results.length-1].key}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>}
                     </div>
 
                     <div className='movie-cast'>
-                        {castInfo && <h2>Top Cast</h2>}
-                        {castInfo && castInfo.cast.map((actor, index) => {
-                            if (index < 10) {
-                                return (
-                                    <div key={actor.id} className="actor-portrait">
-                                        {actor.profile_path && <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} />}
-                                        {!actor.profile_path && <img src="/images/user.png" alt={actor.name} />}
-                                        <div className="actor-info">
-                                            <p>{actor.name}</p>
-                                            <p>{actor.character}</p>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        return null;
-                        })}
-                    </div>
+                        {castInfo && <h1>Top Cast</h1>}
+                        <div className="actors-grid">
+                            {castInfo && castInfo.cast.map((actor, index) => {
+                                if (index < 12) {
+                                    return (
+                                            <div key={actor.id} className="actor-portrait">
+                                                {actor.profile_path && <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} />}
+                                                {!actor.profile_path && <img src="/images/user.png" alt={actor.name} />}
+                                                <div className="actor-info">
+                                                    <p>{actor.name}</p>
+                                                    <p>{actor.character}</p>
+                                                </div>
+                                            </div>
+                                    )
+                                }
+                            return null;
+                            })}
+                            </div>
+                        </div>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     )
 }
